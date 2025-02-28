bb0fe4ec3c1c9ca5770ef11cd58bfc05688f26fa
Linnea Närhi <narhi07@gmail.com>
2025-02-28 10:20:40 +0100

    uppdaterat sql-fil

--- server/db/products.sql
+++ server/db/products.sql
@@ -9,6 +9,7 @@ CREATE TABLE products (
     publishDate DATETIME
 );
 
+
 INSERT INTO products (
     productName,
     description,
@@ -18,13 +19,13 @@ INSERT INTO products (
     brand, 
     publishDate
 ) VALUES (
-    'Svart tröja',
+    'Arcs Vase',
     'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
-    'https://placehold.co/600x400',
+    'https://www.hay.com/img_20240820125321/globalassets/inriver/integration/service/arcs-vase_910x1100_brandmodel.jpg?w=1200',
     'AAA123',
-    '999',
-    'Freaky Fashion™',
-    '2025-02-19'
+    '1299',
+    'HAY',
+    '2025-02-18'
 );
 
 INSERT INTO products (
@@ -36,11 +37,407 @@ INSERT INTO products (
     brand, 
     publishDate
 ) VALUES (
-    'Röd tröja',
+    'Empire Vase',
     'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
-    'https://placehold.co/600x400?text=red-shirt',
-    'AAA345',
-    '999',
-    'Freaky Fashion™',
+    'https://www.hay.com/img_20241213081355/globalassets/inriver/integration/service/empire-vase-_910x1100_brandmodel.jpg?w=1200',
+    'AAA123',
+    '709',
+    'HAY',
     '2025-02-18'
+);
+
+INSERT INTO products (
+    productName,
+    description,
+    image,
+    SKU, 
+    price,
+    brand, 
+    publishDate
+) VALUES (
+    'Shadow Vase',
+    'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
+    'https://www.hay.com/img_20240903114411/globalassets/inriver/integration/service/jessica-hans-shadow-vase_910x1100_brandmodel.jpg?w=1200',
+    'AAA123',
+    '1049',
+    'HAY',
+    '2025-02-18'
+);
+
+INSERT INTO products (
+    productName,
+    description,
+    image,
+    SKU, 
+    price,
+    brand, 
+    publishDate
+) VALUES (
+    'Juice Vase',
+    'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
+    'https://www.hay.com/img_20220826103218/globalassets/inriver/integration/service/juice-vase_910x1100_brandmodel.jpg?w=1200',
+    'AAA123',
+    '2299',
+    'HAY',
+    '2025-02-18'
+);
+
+INSERT INTO products (
+    productName,
+    description,
+    image,
+    SKU, 
+    price,
+    brand, 
+    publishDate
+) VALUES (
+    'Morrocan Vase',
+    'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
+    'https://www.hay.com/img_20220826103217/globalassets/inriver/integration/service/moroccan-vase_brandmodel.jpg?w=1200',
+    'AAA123',
+    '2299',
+    'HAY',
+    '2025-02-18'
+);
+
+INSERT INTO products (
+    productName,
+    description,
+    image,
+    SKU, 
+    price,
+    brand, 
+    publishDate
+) VALUES (
+    'Hexagon Candle',
+    'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
+    'https://www.hay.com/img_20240731111528/globalassets/inriver/integration/service/hexagon-candle_910x1100_brandmodel.jpg?w=1200',
+    'AAA123',
+    '189',
+    'HAY',
+    '2025-02-18'
+);
+
+INSERT INTO products (
+    productName,
+    description,
+    image,
+    SKU, 
+    price,
+    brand, 
+    publishDate
+) VALUES (
+    'Flare Candleholder',
+    'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
+    'https://www.hay.com/img_20240903113320/globalassets/inriver/integration/service/flare-candleholder_910x1100_brandmodel2.jpg?w=1200',
+    'AAA123',
+    '329',
+    'HAY',
+    '2025-02-18'
+);
+
+INSERT INTO products (
+    productName,
+    description,
+    image,
+    SKU, 
+    price,
+    brand, 
+    publishDate
+) VALUES (
+    'Arcs candleholder',
+    'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
+    'https://www.hay.com/img_20220826103210/globalassets/inriver/integration/service/arcs-candleholder_910x1100_brandmodel.jpg?w=1200',
+    'AAA123',
+    '399',
+    'HAY',
+    '2025-02-18'
+);
+
+INSERT INTO products (
+    productName,
+    description,
+    image,
+    SKU, 
+    price,
+    brand, 
+    publishDate
+) VALUES (
+    'Soft candleholder',
+    'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
+    'https://www.hay.com/img_20220826103209/globalassets/inriver/integration/service/ws-soft-candleholder_910x1100_brandmodel.jpg?w=1200',
+    'AAA123',
+    '659',
+    'HAY',
+    '2025-02-18'
+);
+
+INSERT INTO products (
+    productName,
+    description,
+    image,
+    SKU, 
+    price,
+    brand, 
+    publishDate
+) VALUES (
+    'Mini Conical candle',
+    'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
+    'https://www.hay.com/img_20220901104454/globalassets/inriver/integration/service/mini-conical-candle_910x1100_brandmodel.jpg?w=1200',
+    'AAA123',
+    '95',
+    'HAY',
+    '2025-02-18'
+);
+
+INSERT INTO products (
+    productName,
+    description,
+    image,
+    SKU, 
+    price,
+    brand, 
+    publishDate
+) VALUES (
+    'Square candle',
+    'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
+    'https://www.hay.com/img_20240731111533/globalassets/inriver/integration/service/square-candle_910x1100_brandmodel.jpg?w=1200',
+    'AAA123',
+    '189',
+    'HAY',
+    '2025-02-18'
+);
+
+INSERT INTO products (
+    productName,
+    description,
+    image,
+    SKU, 
+    price,
+    brand, 
+    publishDate
+) VALUES (
+    'Matin Lamp',
+    'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
+    'https://www.hay.com/img_20240212032119/globalassets/inriver/integration/service/matin-pendant_910x1100_brandmodel.jpg?w=1200',
+    'AAA123',
+    '1688',
+    'HAY',
+    '2025-02-27'
+);
+
+INSERT INTO products (
+    productName,
+    description,
+    image,
+    SKU, 
+    price,
+    brand, 
+    publishDate
+) VALUES (
+    'PC Lamp',
+    'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
+    'https://www.hay.com/img_20220826103324/globalassets/inriver/integration/service/pc-pendant_910x1100_brandmodel.jpg?w=1200',
+    'AAA123',
+    '1688',
+    'HAY',
+    '2025-02-27'
+);
+
+INSERT INTO products (
+    productName,
+    description,
+    image,
+    SKU, 
+    price,
+    brand, 
+    publishDate
+) VALUES (
+    'Nelson Lamp',
+    'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
+    'https://www.hay.com/img_20220826103324/globalassets/inriver/integration/service/nelson-pendant_910x1100_brandmodel.jpg?w=1200',
+    'AAA123',
+    '7299',
+    'HAY',
+    '2025-02-27'
+);
+
+INSERT INTO products (
+    productName,
+    description,
+    image,
+    SKU, 
+    price,
+    brand, 
+    publishDate
+) VALUES (
+    'Apex Lamp',
+    'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
+    'https://www.hay.com/img_20241213081340/globalassets/inriver/integration/service/apex-floor-lamp_910x1100_brandmodel.jpg?w=1200',
+    'AAA123',
+    '3049',
+    'HAY',
+    '2025-02-27'
+);
+
+INSERT INTO products (
+    productName,
+    description,
+    image,
+    SKU, 
+    price,
+    brand, 
+    publishDate
+) VALUES (
+    'Fifty-Fifty Lamp',
+    'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
+    'https://www.hay.com/hay/lighting/floor-lamp/fifty-fifty-floor-lamp',
+    'AAA123',
+    '7299',
+    'HAY',
+    '2025-02-27'
+);
+
+INSERT INTO products (
+    productName,
+    description,
+    image,
+    SKU, 
+    price,
+    brand, 
+    publishDate
+) VALUES (
+    'Pao Steel Lamp',
+    'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
+    'https://www.hay.com/img_20240415114213/globalassets/inriver/integration/service/pao-steel-floor-lamp_910x1100_brandmodel.jpg?w=1200',
+    'AAA123',
+    '5599',
+    'HAY',
+    '2025-02-27'
+);
+
+INSERT INTO products (
+    productName,
+    description,
+    image,
+    SKU, 
+    price,
+    brand, 
+    publishDate
+) VALUES (
+    'LBM Lamp',
+    'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
+    'https://www.hay.com/img_20240820125324/globalassets/inriver/integration/service/lbm-table-lamp_910x1100_brandmodel2.jpg?w=1200',
+    'AAA123',
+    '2049',
+    'HAY',
+    '2025-02-27'
+);
+
+INSERT INTO products (
+    productName,
+    description,
+    image,
+    SKU, 
+    price,
+    brand, 
+    publishDate
+) VALUES (
+    'Matin Table Lamp',
+    'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
+    'https://www.hay.com/img_20220826103322/globalassets/inriver/integration/service/matin-table-lamp_910x1100_brandmodel.jpg?w=1200',
+    'AAA123',
+    '2889',
+    'HAY',
+    '2025-02-27'
+);
+
+INSERT INTO products (
+    productName,
+    description,
+    image,
+    SKU, 
+    price,
+    brand, 
+    publishDate
+) VALUES (
+    'Shim Coffee Table',
+    'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
+    'https://www.hay.com/img_20240731011129/globalassets/inriver/integration/service/shim-coffee-table_910x1100_brandmodel.jpg?w=1200',
+    'AAA123',
+    '2399',
+    'HAY',
+    '2025-03-04'
+);
+
+INSERT INTO products (
+    productName,
+    description,
+    image,
+    SKU, 
+    price,
+    brand, 
+    publishDate
+) VALUES (
+    'Arcs Side Table ',
+    'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
+    'https://www.hay.com/img_20230720125220/globalassets/inriver/integration/service/arcs-trolley_910x1100_brandmodel.jpg?w=1200',
+    'AAA123',
+    '1688',
+    'HAY',
+    '2025-03-04'
+);
+
+INSERT INTO products (
+    productName,
+    description,
+    image,
+    SKU, 
+    price,
+    brand, 
+    publishDate
+) VALUES (
+    'Kofi Coffee Table ',
+    'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
+    'https://www.hay.com/img_20220826103148/globalassets/inriver/integration/service/kofi_910x1100_brandmodel.jpg?w=1200',
+    'AAA123',
+    '5749',
+    'HAY',
+    '2025-03-04'
+);
+
+INSERT INTO products (
+    productName,
+    description,
+    image,
+    SKU, 
+    price,
+    brand, 
+    publishDate
+) VALUES (
+    'Rebar Side Table ',
+    'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
+    'https://www.hay.com/img_20230126125122/globalassets/inriver/integration/service/rebar-side-table_910x1100_brandmodel3.jpg?w=1200',
+    'AAA123',
+    '4099',
+    'HAY',
+    '2025-03-04'
+);
+
+INSERT INTO products (
+    productName,
+    description,
+    image,
+    SKU, 
+    price,
+    brand, 
+    publishDate
+) VALUES (
+    'Bella Coffee Table ',
+    'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
+    'https://www.hay.com/img_20231207015130/globalassets/inriver/integration/service/bella_910x1100_brandmodel.jpg?w=1200',
+    'AAA123',
+    '3399',
+    'HAY',
+    '2025-03-04'
 );
