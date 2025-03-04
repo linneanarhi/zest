8e04ccaa6bb44eb09da9d46ed10ff81b8c277476
Lovisa Palez <lovisa.palez@gmail.com>
2025-03-04 10:27:14 +0100

    glömde skapa ny branch för detaljsidan...

---
+++ client/src/components/ProductDetails/ProductDetails.jsx
@@ -0,0 +1,14 @@
+import {Link} from 'react-router';
+import { useState, useEffect } from 'react';
+import '../../App.css'
+import styles from "./ProductDetails.module.css";
+
+
+function productDetails() {
+
+    return (
+        <>
+
+        </>
+    )
+}
