bdfdabf8473287aa7bde795db6bc1dfaad68cd86
Linnea Närhi <narhi07@gmail.com>
2025-03-05 15:29:55 +0100

    accordion fixad

---
+++ client/src/components/Accordion/accordion.jsx
@@ -0,0 +1,44 @@
+import styles from './accordion.module.css';
+import { useState } from 'react';
+
+function Accordion() {
+  const accordionData = [
+    {
+      title: 'About',
+      content: 'Our story'
+    },
+    {
+      title: 'My pages',
+      content: 'Log in'
+    },
+    {
+      title: 'Contact',
+      content: 'Return Policy\nContact Us'
+    }
+  ];
+
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const toggleAccordion = index => {
+    setActiveIndex(activeIndex === index ? null : index); {/*activeIndex håller koll på vilken som är öppen (aktiv) så bara den visas */}
+  };
+
+  return (
+    <div className={styles.accordion}>
+      {accordionData.map((item, index) => (
+        <div key={index} className={styles.accordionItem}>
+          <div
+            className={styles.accordionTitle}
+            onClick={() => toggleAccordion(index)}
+          >
+            <div>{item.title}</div>
+            <div>{activeIndex === index ? '-' : '+'}</div>
+          </div>
+          {activeIndex === index && <div className={styles.accordionContent}>{item.content}</div>}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Accordion;
--- client/src/components/Footer/footer.jsx
+++ client/src/components/Footer/footer.jsx
@@ -1,8 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInstagram, faSquareFacebook, faLinkedin, faTiktok } from "@fortawesome/free-brands-svg-icons";
 import styles from './footer.module.css';
-import Accordion from 'react-bootstrap/Accordion';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Accordion from '../Accordion/accordion'
 
 function Footer () {
 
@@ -20,38 +19,8 @@ function Footer () {
         <FontAwesomeIcon icon={faTiktok} />
         </div>
 
-        <div className={styles.mobileAccordion}>
-        <Accordion>
-      <Accordion.Item  eventKey="0" >
-        <Accordion.Header >About</Accordion.Header>
-        <Accordion.Body>
-          <ul>
-            <li>Our Story</li>
-          </ul>
-          
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="1">
-        <Accordion.Header>My Pages</Accordion.Header>
-        <Accordion.Body>
-          <ul>
-            <li>Log In</li>
-          </ul>
-          
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="2">
-        <Accordion.Header>Contact</Accordion.Header>
-        <Accordion.Body>
-          <ul>
-            <li>Return Policy</li>
-            <li>Contact Us</li>
-          </ul>
-          
-        </Accordion.Body>
-      </Accordion.Item>
-    </Accordion>
-    </div>
+{/*ACCORDION*/}
+<Accordion />
 
       
     <div className={styles.footerList}>
