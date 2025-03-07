1aa260c909891ce6158c9641c298b5b0ebc634b3
Linnea Närhi <narhi07@gmail.com>
2025-03-07 14:04:56 +0100

    hjärtikon på bilden fixad

--- client/src/components/products/products.jsx
+++ client/src/components/products/products.jsx
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom'; 
 import styles from './products.module.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
 
 function ProductList({ products }) {
   return (
@@ -11,9 +13,12 @@ function ProductList({ products }) {
               <img className={styles.productImage} src={product.image} alt={product.productName} />
               <div className={styles.overlay}>
                 <h2>{product.productName}</h2>
-                <span>{product.price} sek</span>        
+                <span>{product.price} sek</span>
               </div>
             </Link>
+            <button className={styles.favoriteButton}>
+              <FontAwesomeIcon icon={faHeart} />
+            </button>
           </div>
         ))}
       </div>
