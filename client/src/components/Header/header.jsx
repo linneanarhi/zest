ead1e9ecab19c92b6f370e3b66478a42d335cc33
Linnea Närhi <narhi07@gmail.com>
2025-03-07 15:14:28 +0100

    sökruta klar. mobilläge startsida klar.

--- client/src/components/Header/header.jsx
+++ client/src/components/Header/header.jsx
@@ -6,54 +6,74 @@ import {
   faHeart,
   faMagnifyingGlass,
 } from "@fortawesome/free-solid-svg-icons";
-import { Link } from 'react-router-dom'; 
+import { Link } from "react-router-dom";
 
 function Header() {
   const [isOpen, setIsOpen] = useState(false);
-  //Returnerar JSX
+  const [searchOpen, setSearchOpen] = useState(false); // Hanterar synligheten av sökrutan
+
   return (
     <>
-      {/*MOBIL*/}
+      {/* MOBIL */}
       <nav className={styles.navbar}>
-        
-          {/* Hamburger button (for mobile) */}
-          <button
-            className={styles.navbarToggle}
-            onClick={() => setIsOpen(!isOpen)} title="Meny">
-            {isOpen ? "✖" : "☰"}
-            
-          </button>
-          
-          <div className={styles.imageLogo}>
+        {/* Hamburger button (for mobile) */}
+        <button
+          className={styles.navbarToggle}
+          onClick={() => setIsOpen(!isOpen)}
+          title="Meny"
+        >
+          {isOpen ? "✖" : "☰"}
+        </button>
+
+        <div className={styles.imageLogo}>
           <Link className={styles.productLink} to={`/`}>
-          <img className={styles.logo} src="/image/zest3.png" alt="ZEST" title="Startsida"/>
+            <img
+              className={styles.logo}
+              src="/image/zest3.png"
+              alt="ZEST"
+              title="Startsida"
+            />
           </Link>
-          </div>
+        </div>
+
+        <div className={styles.headerIcon}>
+          <FontAwesomeIcon
+            icon={faBagShopping}
+            className={styles.icon}
+            title="Varukorg"
+          />
+          <FontAwesomeIcon
+            icon={faHeart}
+            className={styles.icon}
+            title="Favoriter"
+          />
+
+          {/* Sökrutan */}
+          {searchOpen && (
+            <input 
+              type="text" 
+              placeholder="Sök..." 
+              className={`${styles.searchInput} ${searchOpen ? styles.open : ""}`}/>
+          )}
           
-          <div className={styles.headerIcon}>
-            <FontAwesomeIcon icon={faBagShopping} className={styles.icon} title="Varukorg" />
-            <FontAwesomeIcon icon={faHeart} className={styles.icon} title="Favoriter"/>
-
-            <input type="text" placeholder="search.." />
-            <FontAwesomeIcon icon={faMagnifyingGlass} className={styles.icon} title="Sök"/>
-          </div>
-
-          {/* Menu items */}
-          <ul
-            className={`${styles["navbarMenu"]} ${isOpen ? styles.open : ""}`}
+          <div 
+            className={styles.icon}
+            onClick={() => setSearchOpen(!searchOpen)}
           >
-            
-              <li>Nyheter</li>
-              <li>Inredning</li>
-              <li>Belysning</li>
-              <li>Möbler</li>
-            
-          </ul>
-        
+            {searchOpen ? "X" : <FontAwesomeIcon icon={faMagnifyingGlass} />}
+          </div>
+        </div>
+
+        {/* Menu items */}
+        <ul className={`${styles["navbarMenu"]} ${isOpen ? styles.open : ""}`}>
+          <li>Nyheter</li>
+          <li>Inredning</li>
+          <li>Belysning</li>
+          <li>Möbler</li>
+        </ul>
       </nav>
 
-      {/*DESKTOP*/}
-
+      {/* DESKTOP */}
       <div className={styles.catagoryDesktop}>
         <ul>
           <li>Nyheter</li>
