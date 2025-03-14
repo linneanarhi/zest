6ee4dc3e8a5128369cf914a24a1feb39d65c1ce2
Lovisa Palez <lovisa.palez@gmail.com>
2025-03-14 17:23:18 +0100

    lagt till 3 bilder under videon i hero

--- client/src/components/Hero/hero.jsx
+++ client/src/components/Hero/hero.jsx
@@ -37,7 +37,31 @@ function Hero() {
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi labore explicabo aliquam illum, doloribus voluptate inventore velit officia. Nemo, consectetur. Consectetur fuga nihil at autem ratione maiores eius odio aperiam! </p>
         </div>
 
-      </div>    
+      </div>   
+
+      <div className={styles.inspoProduct}>
+  <div className={styles.inspoItem}>
+    <img src="https://www.hay.com/globalassets/blocks/brandsite/inspiration/the-hay-vase-collection/2024/arcs-vase-white_arcs-trolley_910x1100.jpg?w=800" alt="Spring 2025" />
+    <div className={styles.inspoOverlay}>
+      <h2>Spring 2025</h2>
+    </div>
+  </div>
+
+  <div className={styles.inspoItem}>
+    <img src="https://www.hay.com/img_20240909014215/globalassets/inriver/integration/service/colour-cabinet-tall_910x1100_brandmodel2.jpg?w=1200" alt="Inspiration" />
+    <div className={styles.inspoOverlay}>
+      <h2>Inspiration</h2>
+    </div>
+  </div>
+
+  <div className={styles.inspoItem}>
+    <img src="https://www.hay.com/img_20220826103333/globalassets/inriver/integration/service/check-bath-mat_910x1100_brandmodel.jpg?w=1200" alt="Coming Soon" />
+    <div className={styles.inspoOverlay}>
+      <h2>Coming Soon</h2>
+    </div>
+  </div>
+</div> 
+       
            
     </>
   );
