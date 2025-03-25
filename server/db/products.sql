3a50ac81d881e58f6fe621532dd5297f9c370ec1
Linnea Närhi <narhi07@gmail.com>
2025-03-25 13:37:28 +0100

    db uppdaterad

--- server/db/products.sql
+++ server/db/products.sql
@@ -2,13 +2,14 @@
 
 CREATE TABLE products (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    productName TEXT UNIQUE REQUIRED,
+    productName TEXT UNIQUE NOT NULL,
     description TEXT,
-    image TEXT REQUIRED UNIQUE,
-    SKU TEXT REQUIRED, 
-    price INTEGER ,
-    brand TEXT ,
-    publishDate INTEGER
+    image TEXT NOT NULL UNIQUE,
+    SKU TEXT NOT NULL, 
+    price INTEGER,
+    brand TEXT,
+    publishDate INTEGER,
+    category TEXT NOT NULL CHECK (category IN ('Lighting', 'Interior', 'Furniture'))
 );
 
 
@@ -19,15 +20,17 @@ INSERT INTO products (
     SKU, 
     price,
     brand, 
-    publishDate
+    publishDate,
+    category
 ) VALUES (
     'Arcs Vase',
     'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
     'https://www.hay.com/img_20240820125321/globalassets/inriver/integration/service/arcs-vase_910x1100_brandmodel.jpg?w=1200',
     'AAA123',
-    '1299',
+    1299,
     'HAY',
-    '1710355200'
+    1710355200,
+    'Interior'
 );
 
 INSERT INTO products (
@@ -37,15 +40,17 @@ INSERT INTO products (
     SKU, 
     price,
     brand, 
-    publishDate
+    publishDate,
+    category
 ) VALUES (
     'Empire Vase',
     'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
     'https://www.hay.com/img_20241213081355/globalassets/inriver/integration/service/empire-vase-_910x1100_brandmodel.jpg?w=1200',
     'AAA123',
-    '709',
+    709,
     'HAY',
-    '1710355200'
+    1710355200,
+    'Interior'
 );
 
 INSERT INTO products (
@@ -55,15 +60,17 @@ INSERT INTO products (
     SKU, 
     price,
     brand, 
-    publishDate
+    publishDate,
+    category
 ) VALUES (
     'Shadow Vase',
     'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
     'https://www.hay.com/img_20240903114411/globalassets/inriver/integration/service/jessica-hans-shadow-vase_910x1100_brandmodel.jpg?w=1200',
     'AAA123',
-    '1049',
+    1049,
     'HAY',
-    '1710355200'
+    1710355200,
+    'Interior'
 );
 
 INSERT INTO products (
@@ -73,15 +80,17 @@ INSERT INTO products (
     SKU, 
     price,
     brand, 
-    publishDate
+    publishDate,
+    category
 ) VALUES (
     'Juice Vase',
     'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
     'https://www.hay.com/img_20220826103218/globalassets/inriver/integration/service/juice-vase_910x1100_brandmodel.jpg?w=1200',
-    'AAA123',
+    AAA123,
     '2299',
     'HAY',
-    '1710355200'
+    1710355200,
+    'Interior'
 );
 
 INSERT INTO products (
@@ -91,15 +100,17 @@ INSERT INTO products (
     SKU, 
     price,
     brand, 
-    publishDate
+    publishDate,
+    category
 ) VALUES (
     'Morrocan Vase',
     'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
     'https://www.hay.com/img_20220826103217/globalassets/inriver/integration/service/moroccan-vase_brandmodel.jpg?w=1200',
     'AAA123',
-    '2299',
+    2299,
     'HAY',
-    '1710355200'
+    1710355200,
+    'Interior'
 );
 
 INSERT INTO products (
@@ -109,15 +120,17 @@ INSERT INTO products (
     SKU, 
     price,
     brand, 
-    publishDate
+    publishDate,
+    category
 ) VALUES (
     'Hexagon Candle',
     'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
     'https://www.hay.com/img_20240731111528/globalassets/inriver/integration/service/hexagon-candle_910x1100_brandmodel.jpg?w=1200',
     'AAA123',
-    '189',
+    189,
     'HAY',
-    '1710355200'
+    1710355200,
+    'Interior'
 );
 
 INSERT INTO products (
@@ -127,15 +140,17 @@ INSERT INTO products (
     SKU, 
     price,
     brand, 
-    publishDate
+    publishDate,
+    category
 ) VALUES (
     'Flare Candleholder',
     'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
     'https://www.hay.com/img_20240903113320/globalassets/inriver/integration/service/flare-candleholder_910x1100_brandmodel2.jpg?w=1200',
     'AAA123',
-    '329',
+    329,
     'HAY',
-    '1710355200'
+    1710355200,
+    'Interior'
 );
 
 INSERT INTO products (
@@ -145,15 +160,17 @@ INSERT INTO products (
     SKU, 
     price,
     brand, 
-    publishDate
+    publishDate,
+    category
 ) VALUES (
     'Arcs candleholder',
     'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
     'https://www.hay.com/img_20220826103210/globalassets/inriver/integration/service/arcs-candleholder_910x1100_brandmodel.jpg?w=1200',
     'AAA123',
-    '399',
+    399,
     'HAY',
-    '1710355200'
+    1710355200,
+    'Interior'
 );
 
 INSERT INTO products (
@@ -163,15 +180,17 @@ INSERT INTO products (
     SKU, 
     price,
     brand, 
-    publishDate
+    publishDate,
+    category
 ) VALUES (
     'Soft candleholder',
     'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
     'https://www.hay.com/img_20220826103209/globalassets/inriver/integration/service/ws-soft-candleholder_910x1100_brandmodel.jpg?w=1200',
     'AAA123',
-    '659',
+    659,
     'HAY',
-    '1710355200'
+    1710355200,
+    'Interior'
 );
 
 INSERT INTO products (
@@ -181,15 +200,17 @@ INSERT INTO products (
     SKU, 
     price,
     brand, 
-    publishDate
+    publishDate,
+    category
 ) VALUES (
     'Mini Conical candle',
     'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
     'https://www.hay.com/img_20220901104454/globalassets/inriver/integration/service/mini-conical-candle_910x1100_brandmodel.jpg?w=1200',
     'AAA123',
-    '95',
+    95,
     'HAY',
-    '1710355200'
+    1710355200,
+    'Interior'
 );
 
 INSERT INTO products (
@@ -199,15 +220,17 @@ INSERT INTO products (
     SKU, 
     price,
     brand, 
-    publishDate
+    publishDate,
+    category
 ) VALUES (
     'Square candle',
     'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
     'https://www.hay.com/img_20240731111533/globalassets/inriver/integration/service/square-candle_910x1100_brandmodel.jpg?w=1200',
     'AAA123',
-    '189',
+    189,
     'HAY',
-    '1710355200'
+    1710355200,
+    'Interior'
 );
 
 INSERT INTO products (
@@ -217,15 +240,17 @@ INSERT INTO products (
     SKU, 
     price,
     brand, 
-    publishDate
+    publishDate,
+    category
 ) VALUES (
     'Matin Lamp',
     'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
     'https://www.hay.com/img_20240212032119/globalassets/inriver/integration/service/matin-pendant_910x1100_brandmodel.jpg?w=1200',
     'AAA123',
-    '1688',
+    1688,
     'HAY',
-    '1710355200'
+    1710355200,
+    'Lighting'
 );
 
 INSERT INTO products (
@@ -235,15 +260,17 @@ INSERT INTO products (
     SKU, 
     price,
     brand, 
-    publishDate
+    publishDate,
+    category
 ) VALUES (
     'PC Lamp',
     'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
     'https://www.hay.com/img_20220826103324/globalassets/inriver/integration/service/pc-pendant_910x1100_brandmodel.jpg?w=1200',
     'AAA123',
-    '1688',
+    1688,
     'HAY',
-    '1710355200'
+    1710355200,
+    'Lighting'
 );
 
 INSERT INTO products (
@@ -253,15 +280,17 @@ INSERT INTO products (
     SKU, 
     price,
     brand, 
-    publishDate
+    publishDate,
+    category
 ) VALUES (
     'Nelson Lamp',
     'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
     'https://www.hay.com/img_20220826103324/globalassets/inriver/integration/service/nelson-pendant_910x1100_brandmodel.jpg?w=1200',
     'AAA123',
-    '7299',
+    7299,
     'HAY',
-    '1710355200'
+    1710355200,
+    'Lighting'
 );
 
 INSERT INTO products (
@@ -271,15 +300,17 @@ INSERT INTO products (
     SKU, 
     price,
     brand, 
-    publishDate
+    publishDate,
+    category
 ) VALUES (
     'Apex Lamp',
     'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
     'https://www.hay.com/img_20241213081340/globalassets/inriver/integration/service/apex-floor-lamp_910x1100_brandmodel.jpg?w=1200',
     'AAA123',
-    '3049',
+    3049,
     'HAY',
-    '1710355200'
+    1710355200,
+    'Lighting'
 );
 
 INSERT INTO products (
@@ -289,15 +320,17 @@ INSERT INTO products (
     SKU, 
     price,
     brand, 
-    publishDate
+    publishDate,
+    category
 ) VALUES (
     'Pao Steel Lamp',
     'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
     'https://www.hay.com/img_20240415114213/globalassets/inriver/integration/service/pao-steel-floor-lamp_910x1100_brandmodel.jpg?w=1200',
     'AAA123',
-    '5599',
+    5599,
     'HAY',
-    '1710355200'
+    1710355200,
+    'Lighting'
 );
 
 INSERT INTO products (
@@ -307,15 +340,17 @@ INSERT INTO products (
     SKU, 
     price,
     brand, 
-    publishDate
+    publishDate,
+    category
 ) VALUES (
     'LBM Lamp',
     'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
     'https://www.hay.com/img_20240820125324/globalassets/inriver/integration/service/lbm-table-lamp_910x1100_brandmodel2.jpg?w=1200',
     'AAA123',
-    '2049',
+    2049,
     'HAY',
-    '1710355200'
+    1710355200,
+    'Lighting'
 );
 
 INSERT INTO products (
@@ -325,15 +360,17 @@ INSERT INTO products (
     SKU, 
     price,
     brand, 
-    publishDate
+    publishDate,
+    category
 ) VALUES (
     'Matin Table Lamp',
     'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
     'https://www.hay.com/img_20220826103322/globalassets/inriver/integration/service/matin-table-lamp_910x1100_brandmodel.jpg?w=1200',
     'AAA123',
-    '2889',
+    2889,
     'HAY',
-    '1710355200'
+    1710355200,
+    'Lighting'
 );
 
 INSERT INTO products (
@@ -343,15 +380,17 @@ INSERT INTO products (
     SKU, 
     price,
     brand, 
-    publishDate
+    publishDate,
+    category
 ) VALUES (
     'Shim Coffee Table',
     'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
     'https://www.hay.com/img_20240731011129/globalassets/inriver/integration/service/shim-coffee-table_910x1100_brandmodel.jpg?w=1200',
     'AAA123',
-    '2399',
+    2399,
     'HAY',
-    '1710355200'
+    1710355200,
+    'Furniture'
 );
 
 INSERT INTO products (
@@ -361,15 +400,17 @@ INSERT INTO products (
     SKU, 
     price,
     brand, 
-    publishDate
+    publishDate,
+    category
 ) VALUES (
     'Arcs Side Table ',
     'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
     'https://www.hay.com/img_20230720125220/globalassets/inriver/integration/service/arcs-trolley_910x1100_brandmodel.jpg?w=1200',
     'AAA123',
-    '1688',
+    1688,
     'HAY',
-    '1710355200'
+    1710355200,
+    'Furniture'
 );
 
 INSERT INTO products (
@@ -379,15 +420,17 @@ INSERT INTO products (
     SKU, 
     price,
     brand, 
-    publishDate
+    publishDate,
+    category
 ) VALUES (
     'Kofi Coffee Table ',
     'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
     'https://www.hay.com/img_20220826103148/globalassets/inriver/integration/service/kofi_910x1100_brandmodel.jpg?w=1200',
     'AAA123',
-    '5749',
+    5749,
     'HAY',
-    '1710355200'
+    1710355200,
+    'Furniture'
 );
 
 INSERT INTO products (
@@ -397,15 +440,17 @@ INSERT INTO products (
     SKU, 
     price,
     brand, 
-    publishDate
+    publishDate,
+    category
 ) VALUES (
     'Rebar Side Table ',
     'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
     'https://www.hay.com/img_20230126125122/globalassets/inriver/integration/service/rebar-side-table_910x1100_brandmodel3.jpg?w=1200',
     'AAA123',
-    '4099',
+    4099,
     'HAY',
-    '1710355200'
+    1710355200,
+    'Furniture'
 );
 
 INSERT INTO products (
@@ -415,15 +460,17 @@ INSERT INTO products (
     SKU, 
     price,
     brand, 
-    publishDate
+    publishDate,
+    category
 ) VALUES (
     'Bella Coffee Table ',
     'lorem ipsum bla bla bla bla hfajkhfjsakjhfsa fasfsa',
     'https://www.hay.com/img_20231207015130/globalassets/inriver/integration/service/bella_910x1100_brandmodel.jpg?w=1200',
     'AAA123',
-    '3399',
+    3399,
     'HAY',
-    '1710355200'
+    1710355200,
+    'Furniture'
 );
 
 ALTER TABLE products ADD COLUMN slug TEXT;
